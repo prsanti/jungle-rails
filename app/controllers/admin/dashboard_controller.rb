fbff75a1acc7a6941d199d6eaee4c5d6687ad1c9
Paulo Santiago <p.santiago796@gmail.com>
2021-01-11 22:26:29 +0000

    create rspec tests for new product

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,8 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
   
-  p Product.count
-  p Category.count
+  # p Product.count
+  # p Category.count
   def show
     @productCount = Product.count
     @categoryCount = Category.count
