94ef6d849710daa9273d0a970b76fb95db07c18e
Paulo Santiago <p.santiago796@gmail.com>
2021-01-12 02:39:32 +0000

    add tests for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,30 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, confirmation: true, length: { minimum: 5 }
   validates :password_confirmation, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
 
+  # before_save do
+  #   self.email.downcase.strip if self.email
+  #   # p self.email
+  # end
+
+  before_save :downcase_email
+
+  def downcase_email
+    self.email.downcase!
+  end
+
+
   def self.authenticate_with_credentials(email, password)
-    user = User.find_by_email(email)
-    if user && user.authenticate(password)
-      return user
+    @user = User.find_by_email(email.downcase.strip)
+    if @user && @user.authenticate(password)
+      @user
     else
-      return nil
+      nil
     end
   end
 end
